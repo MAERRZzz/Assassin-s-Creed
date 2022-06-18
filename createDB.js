c231f892873d9ace7bef6a9bae604b9e3b00ede9
MAERZzz <ernest.mamutov.03@mail.ru>
2022-06-18 14:35:03 +0300

    заполнение БД с mongoose

--- createDB.js
+++ createDB.js
@@ -13,18 +13,50 @@
 //         client.close();
 //     })
 // })
+var async = require('async');
+
 var mongoose = require('mongoose')
 mongoose.connect('mongodb://localhost/AC')
 var Hero = require("./models/hero").Hero
 
-var hero = new Hero({
-    title: "Катерина Сфорца",
-    nick: "Ассасин"
+mongoose.connection.on("open",function(){
+    var db = mongoose.connection.db
+    db.dropDatabase(function(err){
+        if(err) throw err
+
+        async.parallel([
+            function(callback){
+                var pig = new Hero({nick:"pig"})
+                pig.save(function(err,pig){
+                    callback(err,"pig")
+                })
+            },
+            function(callback){
+                var vinni = new Hero({nick:"vinni"})
+                vinni.save(function(err,vinni){
+                    callback(err,"vinni")
+                })
+            },
+            function(callback){
+                var sova = new Hero({nick:"sova"})
+                sova.save(function(err,sova){
+                    callback(err,"sova")
+                })
+            }
+        ],
+        function(err,result){
+            if(err){
+                console.log(err)
+            } else {
+                console.log("Успешно созданы герои с никами: " +result.join(", "))
+            }
+            mongoose.disconnect()
+        })
+    })
 })
 
-hero.save(function(err, hero, affected){
-    console.log(hero.title)
-})
+
+
 
 
 
