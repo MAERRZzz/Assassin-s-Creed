285d0c09c7d31de918a2acb0762163a1456341a5
MAERZzz <ernest.mamutov.03@mail.ru>
2022-06-18 18:11:57 +0300

    заполнение меню из БД

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,16 @@
-var express = require('express');
-var router = express.Router();
+var express = require('express')
+var router = express.Router()
+var Hero = require("../models/hero").Hero
+
 
 /* GET home page. */
-router.get('/', function (req, res, next) {
-    res.render('index', { title: 'Assassin\'s Creed' });
+router.get('/', function(req, res, next) {
+    Hero.find({},{_id:0,title:1,nick:1},function(err,menu){
+        res.render('index', {
+            title: 'Assassin\'s Creed',
+            menu: menu
+        });
+    })
 });
 
 module.exports = router;
