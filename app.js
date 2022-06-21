ff5455d17c656ff581782e546bb4dc92d21c4bed
MAERZzz <ernest.mamutov.03@mail.ru>
2022-06-21 17:38:16 +0300

    сохранение session в mongodb

--- app.js
+++ app.js
@@ -3,37 +3,36 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-var session = require("express-session")
-
-
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
 var heroes = require('./routes/heroes');
+
+var routes = require('./routes/index');
+var users = require('./routes/users');
 var mongoose = require('mongoose')
 mongoose.connect('mongodb://localhost/AC')
-
+var session = require('express-session');
+var MongoStore = require('connect-mongo');
 
 var app = express();
 
 // view engine setup
+app.engine('ejs', require('ejs-locals'));
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
-
+app.use('/heroes', heroes);
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-
+app.use(express.static(path.join(__dirname, 'bower_components')));
 app.use(session({
   secret: "AC",
-  cookie:{maxAge:60*1000}
+  cookie: { maxAge: 60 * 1000 },
+  store: MongoStore.create({ mongoUrl: 'mongodb://localhost/AC' })
 }))
 
-
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-app.use('/heroes', heroes);
+app.use('/', routes);
+app.use('/users', users);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/index.js
+++ routes/index.js
@@ -4,16 +4,14 @@ var Hero = require("../models/hero").Hero
 
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-    Hero.find({},{_id:0,title:1,nick:1},function(err,menu){
+router.get('/', function (req, res, next) {
+    Hero.find({}, { _id: 0, title: 1, nick: 1 }, function (err, menu) {
+        req.session.greeting = "Hi!!!!"
         res.render('index', {
             title: 'Assassin\'s Creed',
             menu: menu
         });
     })
-});
-router.get('/', function(req, res, next) {
-    res.cookie('greeting', 'Hi!!!').render('index', { title: 'Assassin\'s Creed', menu:menu });
 });
 
 module.exports = router;
