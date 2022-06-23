6b0341bbf35e543af0e2370cceaaba8ccd9d3b80
MAERZzz <ernest.mamutov.03@mail.ru>
2022-06-23 18:31:07 +0300

    логика пользователя

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var router = express.Router();
+var User = require("./../models/User").User
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
@@ -13,10 +14,28 @@ router.get('/', function (req, res, next) {
 router.get('/logreg', function(req, res, next) {
     res.render('logreg',{title: 'Вход'});
     });   
-router.post('/logreg', function(req, res, next) {
-    var username = req.body.username
-    var password = req.body.password    
-});
-     
+    router.post('/logreg', function(req, res, next) {
+        var username = req.body.username
+        var password = req.body.password
+        User.findOne({username:username},function(err,user){
+            if(err) return next(err)
+            if(user){
+                if(user.checkPassword(password)){
+                    req.session.user = user._id
+                    res.redirect('/')
+                } else {
+                          res.render('logreg')
+                }
+           } else {
+           var user = new User({username:username,password:password})
+                user.save(function(err,user){
+                    if(err) return next(err)
+                    req.session.user = user._id
+                    res.redirect('/')
+                })        
+          }
+        })
+    });
+    
 
 module.exports = router;
