be5faec22b8bfec4597a3114df3b29c970684bdc
MAERZzz <ernest.mamutov.03@mail.ru>
2022-06-23 18:15:23 +0300

    подготовка данных для передачи на сервер

--- routes/index.js
+++ routes/index.js
@@ -12,6 +12,11 @@ router.get('/', function (req, res, next) {
 /* GET login/registration page. */
 router.get('/logreg', function(req, res, next) {
     res.render('logreg',{title: 'Вход'});
-    });    
+    });   
+router.post('/logreg', function(req, res, next) {
+    var username = req.body.username
+    var password = req.body.password    
+});
+     
 
 module.exports = router;
